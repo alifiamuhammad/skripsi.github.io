469fbf99b6b0f4bfdd1361ad9b4fc1d6e720aa8a
alifiamuhammad <72847257+alifiamuhammad@users.noreply.github.com>
2023-01-05 04:51:03 +0700

    a

--- resources/views/tambah_data_siswa.blade.php
+++ resources/views/tambah_data_siswa.blade.php
@@ -71,57 +71,33 @@
        
     </div>
   </div>
+  
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
   <script>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-        $(document).ready(function () {
-            $('#country-dropdown').on('change', function () {
-                var idCountry = this.value;
-                $("#state-dropdown").html('');
-                $.ajax({
-                    url: "{{url('api/fetch-states')}}",
-                    type: "POST",
-                    data: {
-                        country_id: idCountry,
-                        _token: '{{csrf_token()}}'
-                    },
-                    dataType: 'json',
-                    success: function (result) {
-                        $('#state-dropdown').html('<option value="">-- Select State --</option>');
-                        $.each(result.states, function (key, value) {
-                            $("#state-dropdown").append('<option value="' + value
-                                .id + '">' + value.name + '</option>');
-                        });
-                        $('#city-dropdown').html('<option value="">-- Select City --</option>');
-                    }
-                });
-            });
-  
-            /*------------------------------------------
-            --------------------------------------------
-            State Dropdown Change Event
-            --------------------------------------------
-            --------------------------------------------*/
-            $('#state-dropdown').on('change', function () {
-                var idState = this.value;
-                $("#city-dropdown").html('');
-                $.ajax({
-                    url: "{{url('api/fetch-cities')}}",
-                    type: "POST",
-                    data: {
-                        state_id: idState,
-                        _token: '{{csrf_token()}}'
-                    },
-                    dataType: 'json',
-                    success: function (res) {
-                        $('#city-dropdown').html('<option value="">-- Select City --</option>');
-                        $.each(res.cities, function (key, value) {
-                            $("#city-dropdown").append('<option value="' + value
-                                .id + '">' + value.name + '</option>');
-                        });
-                    }
-                });
-            });
-  
-        });
+      $(document).ready(function () {
+          $('#country-dropdown').on('change', function () {
+              var idJurusan = this.value;
+              $("#state-dropdown").html('');
+              $.ajax({
+                  url: "{{url('api/fetch-states')}}",
+                  type: "POST",
+                  data: {
+                      jurusan_id: idCountry,
+                      _token: '{{csrf_token()}}'
+                  },
+                  dataType: 'json',
+                  success: function (result) {
+                      $('#state-dropdown').html('<option value="">-- Select State --</option>');
+                      $.each(result.states, function (key, value) {
+                          $("#state-dropdown").append('<option value="' + value
+                              .id + '">' + value.name + '</option>');
+                      });
+                      $('#city-dropdown').html('<option value="">-- Select City --</option>');
+                  }
+              });
+          });
+
+      });
+ 
     </script>
 @endsection
